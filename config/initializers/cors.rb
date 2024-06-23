ad448230a213e8327ce0e57174a04b73bd6118ae
Takagi Yoshino <yoshino.51007zhu@gmail.com>
2024-06-23 19:44:52 +0900

    fic cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://posts-front-fy60b0l0v-yoshi-projects.vercel.app"
+    origins "http://localhost:3000", "https://posts-front-end.vercel.app"
 
     resource "*",
       headers: :any,
